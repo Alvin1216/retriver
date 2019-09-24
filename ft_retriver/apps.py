a00ee64580c73324e10afe8d8406f8d2b37a8544
Alvin <fea1216@gmail.com>
2019-09-24 16:23:11 +0800

    first commit
    first version of the parser

---
+++ ft_retriver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FtRetriverConfig(AppConfig):
+    name = 'ft_retriver'
---
+++ ft_retriver/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from ft_retriver.parser import python_parser as ps
+
+ps.load_from_file()
+
+# Create your views here.
